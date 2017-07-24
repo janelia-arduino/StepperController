0cf4d059ae2824eb0454f5c0bb79f4d5ff9a3308
Peter Polidoro <peterpolidoro@gmail.com>
2017-07-24 10:49:28 -0400

    Updated 5x3.

--- utility/5x3.cpp
+++ utility/5x3.cpp
@@ -16,9 +16,9 @@ namespace constants
 {
 const size_t cs_pins[DRIVER_COUNT] =
   {
-    14,
-    15,
     16,
+    17,
+    18,
   };
 
 // Interrupts
@@ -26,6 +26,96 @@ const size_t cs_pins[DRIVER_COUNT] =
 // Units
 
 // Properties
+const double steps_per_position_units_default[DRIVER_COUNT] =
+  {
+    steps_per_position_units_element_default,
+    steps_per_position_units_element_default,
+    steps_per_position_units_element_default,
+  };
+
+const double velocity_max_default[DRIVER_COUNT] =
+  {
+    velocity_max_element_default,
+    velocity_max_element_default,
+    velocity_max_element_default,
+  };
+
+const double velocity_min_default[DRIVER_COUNT] =
+  {
+    velocity_min_element_default,
+    velocity_min_element_default,
+    velocity_min_element_default,
+  };
+
+const double acceleration_max_default[DRIVER_COUNT] =
+  {
+    acceleration_max_element_default,
+    acceleration_max_element_default,
+    acceleration_max_element_default,
+  };
+
+const ConstantString * const enable_polarity_default[DRIVER_COUNT] =
+  {
+    enable_polarity_element_default,
+    enable_polarity_element_default,
+    enable_polarity_element_default,
+  };
+
+const bool left_switch_stop_enabled_default[DRIVER_COUNT] =
+  {
+    left_switch_stop_enabled_element_default,
+    left_switch_stop_enabled_element_default,
+    left_switch_stop_enabled_element_default,
+  };
+
+const bool right_switch_stop_enabled_default[DRIVER_COUNT] =
+  {
+    right_switch_stop_enabled_element_default,
+    right_switch_stop_enabled_element_default,
+    right_switch_stop_enabled_element_default,
+  };
+
+const bool switch_soft_stop_enabled_default[DRIVER_COUNT] =
+  {
+    switch_soft_stop_enabled_element_default,
+    switch_soft_stop_enabled_element_default,
+    switch_soft_stop_enabled_element_default,
+  };
+
+const double home_velocity_default[DRIVER_COUNT] =
+  {
+    home_velocity_element_default,
+    home_velocity_element_default,
+    home_velocity_element_default,
+  };
+
+const long run_current_default[DRIVER_COUNT] =
+  {
+    run_current_element_default,
+    run_current_element_default,
+    run_current_element_default,
+  };
+
+const long hold_current_default[DRIVER_COUNT] =
+  {
+    hold_current_element_default,
+    hold_current_element_default,
+    hold_current_element_default,
+  };
+
+const long hold_delay_default[DRIVER_COUNT] =
+  {
+    hold_delay_element_default,
+    hold_delay_element_default,
+    hold_delay_element_default,
+  };
+
+const long microsteps_per_step_default[DRIVER_COUNT] =
+  {
+    microsteps_per_step_subset[MICROSTEPS_PER_STEP_SUBSET_LENGTH - 1].l,
+    microsteps_per_step_subset[MICROSTEPS_PER_STEP_SUBSET_LENGTH - 1].l,
+    microsteps_per_step_subset[MICROSTEPS_PER_STEP_SUBSET_LENGTH - 1].l,
+  };
 
 // Parameters
 
--- utility/5x3.h
+++ utility/5x3.h
@@ -26,6 +26,31 @@ extern const size_t cs_pins[DRIVER_COUNT];
 
 // Properties
 // Property values must be long, double, bool, long[], double[], bool[], char[], ConstantString *, (ConstantString *)[]
+extern const double steps_per_position_units_default[DRIVER_COUNT];
+
+extern const double velocity_max_default[DRIVER_COUNT];
+
+extern const double velocity_min_default[DRIVER_COUNT];
+
+extern const double acceleration_max_default[DRIVER_COUNT];
+
+extern const ConstantString * const enable_polarity_default[DRIVER_COUNT];
+
+extern const bool left_switch_stop_enabled_default[DRIVER_COUNT];
+
+extern const bool right_switch_stop_enabled_default[DRIVER_COUNT];
+
+extern const bool switch_soft_stop_enabled_default[DRIVER_COUNT];
+
+extern const double home_velocity_default[DRIVER_COUNT];
+
+extern const long run_current_default[DRIVER_COUNT];
+
+extern const long hold_current_default[DRIVER_COUNT];
+
+extern const long hold_delay_default[DRIVER_COUNT];
+
+extern const long microsteps_per_step_default[DRIVER_COUNT];
 
 // Parameters
 
