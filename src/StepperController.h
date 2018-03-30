4c455e754ce0850bcb73f00a9c5d801dc32f2474
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-30 14:12:27 -0400

    Removed unnecessary header includes.

--- src/StepperController.h
+++ src/StepperController.h
@@ -7,27 +7,14 @@
 // ----------------------------------------------------------------------------
 #ifndef STEPPER_CONTROLLER_H
 #define STEPPER_CONTROLLER_H
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
 #include <ArduinoJson.h>
-#include <JsonSanitizer.h>
 #include <JsonStream.h>
 #include <Array.h>
 #include <Vector.h>
-#include <MemoryFree.h>
 #include <ConstantVariable.h>
-#include <SavedVariable.h>
 #include <Functor.h>
-#include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
-#include <Watchdog.h>
 
-#include <SPI.h>
-#include <TMC429.h>
 #include <TMC2130.h>
-
-#include <ModularClient.h>
 
 #include <ModularServer.h>
 #include <ModularDeviceBase.h>
